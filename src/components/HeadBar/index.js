60da1dc88c1991ab6b483c54d70c0b6a3e4ab8a3
Dessinta Rahmadani <50995379+dessintadr@users.noreply.github.com>
2021-09-10 13:25:58 +0700

    Update index.js

--- src/components/HeadBar/index.js
+++ src/components/HeadBar/index.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 export default class HeadBar extends Component {
     render() {
         return (
+       
 <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-gray-300 shadow sm:items-baseline w-full">
   <div class="mb-2 sm:mb-0">
     <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">{this.props.home}</a>
